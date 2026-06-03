5e463adb70e6ce5ae789b11f1480869235f12cf6
Andre K. <andre@glowingmind.net>
2026-06-03 12:52:04 -0500

    Added RGB LED basic control

--- controller-demos/8bitdo-pro2-demo.py
+++ controller-demos/8bitdo-pro2-demo.py
@@ -1,18 +1,61 @@
 # import evdev
 from evdev import InputDevice, categorize, ecodes
+from gpiozero import RGBLED
+from random import Random
+from time import sleep
 
 # create callable object to access controller data
 rmtctl = InputDevice('/dev/input/event7')
 
+# create RGBLED object to control LED colors
+led = RGBLED(18, 23, 24)
+rnd = Random()
+values = [0, 0, 0]
+
 # print out controller data
 print(rmtctl)
+
+def shiftList(l):
+    return l[-1:]+l[:-1]
 
 try:
     # loop to read controller data
     for event in rmtctl.read_loop():
         if event.type == ecodes.EV_KEY:
-            print(categorize(event))
-        elif event.type == ecodes.EV_ABS:
-            print(categorize(event))
+            # print(categorize(event))
+            # print(event.code)
+            if event.code == 305 and event.value == 1:
+                for i, v in enumerate(values):
+                    values[i] = rnd.random()
+                led.pulse(
+                    fade_in_time=1,
+                    fade_out_time=1,
+                    on_color=tuple(values),
+                    off_color=(0, 0, 0),
+                    n=None,
+                    background=True
+                )
+                # led.color = (values[0], values[1], values[2])
+                # print(values)
+                # sleep(3)
+                # values = shiftList(values)
+                # led.color = (values[0], values[1], values[2])
+                # print(values)
+                # sleep(3)
+                # values = shiftList(values)
+                # led.color = (values[0], values[1], values[2])
+                # print(values)
+                # sleep(3)
+            if event.code == 304 and event.value == 1:
+                led.off()
+        # elif event.type == ecodes.EV_ABS:
+        #     print(categorize(event))
+except OSError as e:
+    if e.errno == 19:
+        print("Controller device disconnected")
+    # print(f"Error occurred: {e}")
+    exit(1)
 except KeyboardInterrupt:
-    print("\nExiting program")
+    print("\nKeyboard interrupt received")
+    led.off()
+    exit(0)
