c36c70828e433c26baa7f47ea2c46f38abc0a7f2
Andre K. <andre@glowingmind.net>
2026-06-04 07:43:42 -0500

    Added manual color configuration

--- controller-demos/8bitdo-pro2-demo.py
+++ controller-demos/8bitdo-pro2-demo.py
@@ -11,6 +11,8 @@ rmtctl = InputDevice('/dev/input/event7')
 led = RGBLED(18, 23, 24)
 rnd = Random()
 values = [0, 0, 0]
+isManualConfig = False
+activeValue = 0
 
 # print out controller data
 print(rmtctl)
@@ -24,32 +26,54 @@ try:
         if event.type == ecodes.EV_KEY:
             # print(categorize(event))
             # print(event.code)
-            if event.code == 305 and event.value == 1:
-                for i, v in enumerate(values):
-                    values[i] = rnd.random()
-                led.pulse(
-                    fade_in_time=1,
-                    fade_out_time=1,
-                    on_color=tuple(values),
-                    off_color=(0, 0, 0),
-                    n=None,
-                    background=True
-                )
-                # led.color = (values[0], values[1], values[2])
-                # print(values)
-                # sleep(3)
-                # values = shiftList(values)
-                # led.color = (values[0], values[1], values[2])
-                # print(values)
-                # sleep(3)
-                # values = shiftList(values)
-                # led.color = (values[0], values[1], values[2])
-                # print(values)
-                # sleep(3)
-            if event.code == 304 and event.value == 1:
-                led.off()
-        # elif event.type == ecodes.EV_ABS:
-        #     print(categorize(event))
+            if event.code == 310 and event.value == 1:
+                isManualConfig = not isManualConfig
+                if not isManualConfig:
+                    led.pulse(
+                        fade_in_time=1,
+                        fade_out_time=1,
+                        on_color=tuple(values),
+                        off_color=(0, 0, 0),
+                        n=None,
+                        background=True
+                    )
+                continue
+            if not isManualConfig:
+                if event.code == 305 and event.value == 1:
+                    for i, v in enumerate(values):
+                        values[i] = round(rnd.random(), 2)
+                    led.pulse(
+                        fade_in_time=1,
+                        fade_out_time=1,
+                        on_color=tuple(values),
+                        off_color=(0, 0, 0),
+                        n=None,
+                        background=True
+                    )
+                if event.code == 304 and event.value == 1:
+                    led.off()
+        elif event.type == ecodes.EV_ABS and isManualConfig:
+            # print(event)
+            # print(categorize(event))
+            if event.code == 17 and event.value < 0 and values[activeValue] < 1:
+                values[activeValue] = round(values[activeValue] + 0.01, 2)
+                led.color = (tuple(values))
+                print(values)
+            if event.code == 17 and event.value > 0 and values[activeValue] > 0:
+                values[activeValue] = round(values[activeValue] - 0.01, 2)
+                led.color = (tuple(values))
+                print(values)
+            if event.code == 16 and event.value > 0:
+                activeValue = activeValue + 1
+                if activeValue > 2:
+                    activeValue = 0
+                continue
+            if event.code == 16 and event.value < 0:
+                activeValue = activeValue - 1
+                if activeValue < 0:
+                    activeValue = 2
+                continue
+
 except OSError as e:
     if e.errno == 19:
         print("Controller device disconnected")
